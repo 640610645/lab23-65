8182e9bf8073950a35a1962f54994031f2373c3b
640610645 <nontapan_chanadee@cmu.ac.th>
2023-03-07 15:37:37 +0700

    lab 23 completed

--- average.cpp
+++ average.cpp
@@ -1,1 +1,14 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
 
+int main(int argc, char* argv[]){
+    int sum = 0;
+    if(argc >= 2){
+        for(int i = 1; i < argc; i++) sum += atoi(argv[i]);
+        cout << "---------------------------------\n";
+        cout << "Average of " << argc-1 << " numbers = " << sum/(argc-1) << endl;
+        cout << "---------------------------------\n";
+    }else cout << "Please input numbers to find average."; 
+	return 0;
+}
